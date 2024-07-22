30b7fdc46f7e6e88d204be18219e148fd4f068e3
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-07-22 19:46:11 +0300

    feat: add initial next.js files

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:@next/next/recommended',
+    ],
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    rules: {
+        'react-refresh/only-export-components': [
+            'off',
+            { allowConstantExport: true },
+        ],
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/semi': 'error',
+    },
+}
