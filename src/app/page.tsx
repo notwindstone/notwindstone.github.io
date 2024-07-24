8730134f431da40882123bb74aa680e1739d9414
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-07-24 21:59:48 +0300

    feat: style CKEditor5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import ThemeChanger from "@/components/common/ThemeChanger/ThemeChanger";
+import TextCKEditor from "@/components/common/TextCKEditor/TextCKEditor";
 
 export default function Home() {
     return (
@@ -6,6 +7,7 @@ export default function Home() {
             className="grid place-items-center h-auto p-4 dark:bg-red light:bg-gray-400"
         >
             <ThemeChanger />
+            <TextCKEditor />
         </main>
     );
 }
