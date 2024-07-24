28946f243b8079609e353bfb4a477badc51912e3
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-07-24 21:29:13 +0300

    feat: save code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@ import ThemeChanger from "@/components/common/ThemeChanger/ThemeChanger";
 
 export default function Home() {
     return (
-        <main className="grid place-items-center h-auto p-4 dark:bg-red light:bg-gray-400">
+        <main
+            className="grid place-items-center h-auto p-4 dark:bg-red light:bg-gray-400"
+        >
             <ThemeChanger />
         </main>
     );
